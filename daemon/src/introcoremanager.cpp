bitdefender/hvmi

commit d958c7dd92688bbfb30753f26d147d89a7ca1721

daemon: Fix unused parameter warning

daemon/src/introcoremanager.cpp
@@ -1008,7 +1008,7 @@ void IntrocoreManager::generateSessionId()
 	// sessionId_ = ??
 }
 
-bool IntrocoreManager::injectAgent( const Tool &agent )
+bool IntrocoreManager::injectAgent( const Tool /* &agent */ )
 {
 	bdvmi::logger << bdvmi::ERROR << "Agent injection is not supported yet!" << std::flush;
 

commit 7042145f0b7309d9cd05e55c176c1cf92c1708bb

daemon: Fix a compilation error introduced by commit d958c7d

daemon/src/introcoremanager.cpp
@@ -1008,7 +1008,7 @@ void IntrocoreManager::generateSessionId()
 	// sessionId_ = ??
 }
 
-bool IntrocoreManager::injectAgent( const Tool /* &agent */ )
+bool IntrocoreManager::injectAgent( const Tool & /* agent */ )
 {
 	bdvmi::logger << bdvmi::ERROR << "Agent injection is not supported yet!" << std::flush;
 